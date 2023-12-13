04b07d226711164b6900c3d649c24434039e6624
abinesh-a7 <abiraja9150@gmail.com>
2023-12-13 19:52:26 +0530

    file added

---
+++ script.js
@@ -0,0 +1,6 @@
+let navbar = document.getElementById("navbar");
+let menuicon = document.getElementById("menu-icon");
+
+menuicon.addEventListener("click", () => {
+  navbar.classList.toggle("showmenu")
+});
